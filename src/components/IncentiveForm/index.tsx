4fd45edaa78fb89645a6d1b16e6822c7dca22870
Elias Thompson <elias@vast.ai>
2026-01-13 15:06:01 -0800

    chore: smaller incentives form

--- src/components/IncentiveForm/index.tsx
+++ src/components/IncentiveForm/index.tsx
@@ -13,6 +13,9 @@ export const IncentiveForm = () => {
   const incentives = useSelector(({ info }) => info.incentives);
 
   const cssGlobal = css`
+    :root {
+      --line-height: calc(((var(--bar-height) - var(--padding)) / 3) / 1.75);
+    }
     body,
     #root {
       width: auto;
@@ -35,53 +38,63 @@ export const IncentiveForm = () => {
     align-items: center;
   `;
   const cssRow = css`
-    margin-top: 1rem;
+    margin-top: 0.5rem;
   `;
-  const cssAddIncentiveButton = css`
+  const cssIncentiveButton = css`
     margin-left: 0.5rem;
-  `;
-  const cssRemoveIncentiveButton = css`
-    margin-left: 0.5rem;
+    border-radius: calc(0.4rem / 1.75);
+    font-size: calc(1.3rem / 1.75);
+    height: calc(3rem / 1.75);
+
+    > div {
+      padding: 0px calc(1rem / 1.75);
+    }
   `;
   const cssAmountSpan = css`
-    width: 12rem;
-    padding: 0 2rem 0 0;
-    padding: 0 1rem;
+    width: 7rem;
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75);
     font-size: var(--font-size);
     font-weight: 500;
   `;
   const cssDescriptionSpan = css`
-    width: 25rem;
-    margin-left: 0.5rem;
-    padding: 0 2rem 0 0;
-    padding: 0 1rem;
+    width: 12rem;
+    margin-left: 0.25rem;
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75);
     font-size: var(--font-size);
     font-weight: 500;
   `;
   const cssDurationSpan = css`
-    width: 12rem;
-    margin-left: 0.5rem;
-    padding: 0 2rem 0 0;
-    padding: 0 1rem;
+    width: 7rem;
+    margin-left: 0.25rem;
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75);
     font-size: var(--font-size);
     font-weight: 500;
   `;
   const cssAmountInput = css`
-    width: 12rem;
+    width: 7rem;
     font-size: var(--font-size);
     line-height: var(--line-height);
+    border-radius: calc(0.4rem / 1.75);
+    height: calc(2rem / 1.75);
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75) !important;
   `;
   const cssDescriptionInput = css`
-    width: 25rem;
-    margin-left: 0.5rem;
+    width: 12rem;
+    margin-left: 0.25rem;
     font-size: var(--font-size);
     line-height: var(--line-height);
+    border-radius: calc(0.4rem / 1.75);
+    height: calc(2rem / 1.75);
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75) !important;
   `;
   const cssDurationInput = css`
-    width: 12rem;
-    margin-left: 0.5rem;
+    width: 7rem;
+    margin-left: 0.25rem;
     font-size: var(--font-size);
     line-height: var(--line-height);
+    border-radius: calc(0.4rem / 1.75);
+    height: calc(2rem / 1.75);
+    padding: calc(0.5rem / 1.75) calc(1rem / 1.75) !important;
   `;
 
   const handleAddIncentiveBlur = (id: Incentive['id']) => (event: FocusEvent<HTMLInputElement>) => {
@@ -110,7 +123,7 @@ export const IncentiveForm = () => {
           <span css={cssDescriptionSpan}>Description</span>
           <span css={cssDurationSpan}>Duration (min)</span>
 
-          <TwitchButton css={cssAddIncentiveButton} onClick={handleAddIncentiveClick}>
+          <TwitchButton css={cssIncentiveButton} onClick={handleAddIncentiveClick}>
             +
           </TwitchButton>
         </FlexContainer>
@@ -139,7 +152,7 @@ export const IncentiveForm = () => {
               onBlur={handleAddIncentiveBlur(id)}
             />
 
-            <TwitchButton css={cssRemoveIncentiveButton} onClick={handleRemoveIncentiveClick(id)}>
+            <TwitchButton css={cssIncentiveButton} onClick={handleRemoveIncentiveClick(id)}>
               –
             </TwitchButton>
           </FlexContainer>
--- src/lib/generateLayout.ts
+++ src/lib/generateLayout.ts
@@ -6,7 +6,7 @@ export const canvasWidth = 1920 as const;
 export const canvasHeight = 1080 as const;
 export const barHeight = 120 as const;
 export const canvasPadding = 30 as const;
-export const aspectRatios = [
+export const contentAspectRatios = [
   [16, 9],
   [5, 3],
   [3, 2],
@@ -19,18 +19,27 @@ export const aspectRatios = [
 
 export const transformAspectRatio = ([widthRatio, heightRatio]: [number, number]) => widthRatio / heightRatio;
 
-export type AspectRatioContent = (typeof aspectRatios)[number];
+export type ContentAspectRatio = (typeof contentAspectRatios)[number];
 
-export const aspectRatiosContent = aspectRatios.map(transformAspectRatio);
-export const aspectRatioContentWidest = Math.max(...aspectRatiosContent);
+export const numericContentAspectRatios = contentAspectRatios.map(transformAspectRatio);
+export const widestNumericContentAspectRatio = Math.max(...numericContentAspectRatios);
 export const canvasPaddingTopBottom = canvasPadding * 3;
 export const canvasGap = canvasPadding;
 export const layoutGap = canvasGap / 2;
 export const canvasContentWidth = canvasWidth - canvasPadding * 2;
 export const canvasContentHeight = canvasHeight - barHeight - canvasPaddingTopBottom * 2;
 
+export type LayoutAlignment =
+  | 'center-column'
+  | 'center'
+  | 'left-column'
+  | 'left'
+  | 'right-column'
+  | 'right'
+  | 'space-between';
+
 export interface LayoutConfig {
-  contentMainAspectRatio: AspectRatioContent;
+  contentMainAspectRatio: ContentAspectRatio;
   hasCamera?: boolean;
   hasContentSide?: boolean;
   hasName?: boolean;
@@ -52,70 +61,69 @@ export interface LayoutTransformInfo {
   name?: TransformInfo;
 }
 
-export const generateLayout = (layoutConfigs: LayoutConfig[]) => {
-  const layoutTotal = layoutConfigs.length;
-  console.log('layoutTotal', layoutTotal);
+export const generateLayout = (layoutConfigs: LayoutConfig[] /*, layoutAlignment: LayoutAlignment = 'center' */) => {
+  const layoutConfigsTotal = layoutConfigs.length;
+  console.log('layoutConfigsTotal', layoutConfigsTotal);
 
-  if (!layoutTotal) return [];
+  if (!layoutConfigsTotal) return [];
 
+  // Constants
   const result: LayoutTransformInfo[] = [];
-  const layoutGridUnits = Math.ceil(Math.sqrt(layoutTotal));
-  console.log('layoutGridUnits', layoutGridUnits);
-  const layoutConfigGrid = distributeItems(layoutConfigs);
-  console.log('layoutConfigGrid', JSON.stringify(layoutConfigGrid));
-  const rowTotal = layoutConfigGrid.length;
-  console.log('rowTotal', rowTotal);
-  const layoutCanvasWidth = (canvasContentWidth - (layoutGridUnits - 1) * canvasGap) / layoutGridUnits;
-  const layoutCanvasHeight = (canvasContentHeight - (layoutGridUnits - 1) * canvasGap) / layoutGridUnits;
-  console.log('layoutCanvasHeight', layoutCanvasHeight);
-  const widestContentWidth = layoutCanvasHeight * aspectRatioContentWidest;
-  const sideWidth = layoutCanvasWidth - widestContentWidth - layoutGap;
-  console.log('sideWidth', sideWidth);
+  const canvasGridUnits = Math.ceil(Math.sqrt(layoutConfigsTotal));
+  console.log('canvasGridUnits', canvasGridUnits);
+  const rows = distributeItems(layoutConfigs);
+  console.log('rows', JSON.stringify(rows));
+  const rowsTotal = rows.length;
+  console.log('rowsTotal', rowsTotal);
+  const canvasGridUnitWidth = (canvasContentWidth - (canvasGridUnits - 1) * canvasGap) / canvasGridUnits;
+  const canvasGridUnitHeight = (canvasContentHeight - (canvasGridUnits - 1) * canvasGap) / canvasGridUnits;
+  console.log('canvasGridUnitHeight', canvasGridUnitHeight);
+  const rowHeightMax =
+    canvasGridUnitHeight * (canvasGridUnits / rowsTotal) + (canvasGridUnits / rowsTotal - 1) * canvasGap;
+  console.log('rowHeightMax', rowHeightMax);
+  const widestLayoutContentWidth = canvasGridUnitHeight * widestNumericContentAspectRatio;
+  const layoutSideWidth = canvasGridUnitWidth - widestLayoutContentWidth - layoutGap;
+  console.log('layoutSideWidth', layoutSideWidth);
 
-  const rowHeights = layoutConfigGrid.map((layoutRow, layoutRowIndex) => {
-    const rowLayoutTotal = layoutRow.length;
-    console.log(`row ${layoutRowIndex + 1}`, 'rowLayoutTotal', rowLayoutTotal);
-    const layoutContentData = layoutRow.map(
-      ({ contentMainAspectRatio, hasCamera = true, hasContentSide = false, infoAspectRatio }) => ({
-        contentMainAspectRatio: transformAspectRatio(contentMainAspectRatio),
-        layoutSideWidth: hasCamera || hasContentSide || infoAspectRatio ? layoutGap + sideWidth : 0,
+  // Find widest row
+  const rowWidths = rows.map((rowLayoutConfigs, rowsIndex) => {
+    const { rowContentMainNumericAspectRatio, rowSideWidth } = rowLayoutConfigs.reduce(
+      (acc, { contentMainAspectRatio, hasCamera = true, hasContentSide = false, infoAspectRatio }) => ({
+        rowContentMainNumericAspectRatio:
+          acc.rowContentMainNumericAspectRatio + transformAspectRatio(contentMainAspectRatio),
+        rowSideWidth:
+          acc.rowSideWidth + (hasCamera || hasContentSide || infoAspectRatio ? layoutGap + layoutSideWidth : 0),
       }),
+      { rowContentMainNumericAspectRatio: 0, rowSideWidth: 0 },
     );
-    console.log(`row ${layoutRowIndex + 1}`, 'layoutContentData', JSON.stringify(layoutContentData));
-    const { layoutContentMainAspectRatioTotal, layoutSideWidthTotal } = layoutContentData.reduce(
-      (acc, { contentMainAspectRatio, layoutSideWidth }) => ({
-        layoutContentMainAspectRatioTotal: acc.layoutContentMainAspectRatioTotal + contentMainAspectRatio,
-        layoutSideWidthTotal: acc.layoutSideWidthTotal + layoutSideWidth,
-      }),
-      { layoutContentMainAspectRatioTotal: 0, layoutSideWidthTotal: 0 },
-    );
-    console.log(`row ${layoutRowIndex + 1}`, 'layoutContentMainAspectRatioTotal', layoutContentMainAspectRatioTotal);
-    console.log(`row ${layoutRowIndex + 1}`, 'layoutSideWidthTotal', layoutSideWidthTotal);
-    // const layoutContentWidth = canvasContentWidth - (layoutGridUnits - 1) * canvasGap - layoutSideWidthTotal;
-    const layoutContentWidth = layoutCanvasHeight * aspectRatioContentWidest;
-    console.log(`row ${layoutRowIndex + 1}`, 'layoutContentWidth', layoutContentWidth);
+    console.log(`rows[${rowsIndex}]`, 'rowContentMainNumericAspectRatio', rowContentMainNumericAspectRatio);
+    console.log(`rows[${rowsIndex}]`, 'rowSideWidth', rowSideWidth);
+    const rowContentWidth = canvasGridUnitHeight * rowContentMainNumericAspectRatio;
+    console.log(`rows[${rowsIndex}]`, 'rowContentWidth', rowContentWidth);
 
-    const rowHeight = layoutCanvasWidth / layoutContentMainAspectRatioTotal;
-    console.log('rowHeight', rowHeight);
-
-    return rowHeight;
+    return rowContentWidth + rowSideWidth + (rowsTotal - 1) * canvasGap;
   });
+  console.log('rowWidths', rowWidths);
+  const widestRowWidth = Math.max(...rowWidths);
+  console.log('widestRowWidth', widestRowWidth);
+  // const widestRowIndex = rowWidths.indexOf(widestRowWidth);
+  // console.log('widestRowIndex', widestRowIndex);
 
-  const rowHeightMin = Math.min(layoutCanvasHeight, ...rowHeights);
-  console.log('rowHeightMin', rowHeightMin);
+  // Get row height
+  const widestRowWidthFactor = canvasContentWidth / widestRowWidth;
+  console.log('widestRowWidthFactor', widestRowWidthFactor);
+  const widestRowHeight = widestRowWidthFactor * canvasGridUnitHeight;
+  console.log('widestRowHeight', widestRowHeight);
+  const rowHeight = Math.min(rowHeightMax, widestRowHeight);
+  console.log('rowHeight', rowHeight);
 
+  throw '';
   return result;
 };
 
-// contentMainAspectRatio,
-// hasCamera = true,
-// hasContentSide = false,
-// hasName = true,
-// info,
-
 export const test = generateLayout([
-  // { contentMainAspectRatio: [4, 6], hasCamera: false },
-  { contentMainAspectRatio: [16, 9] },
-  { contentMainAspectRatio: [16, 9] },
+  { contentMainAspectRatio: [4, 6], hasCamera: false },
+  // { contentMainAspectRatio: [4, 3] },
+  // { contentMainAspectRatio: [16, 9] },
   { contentMainAspectRatio: [16, 9] },
 ]);
